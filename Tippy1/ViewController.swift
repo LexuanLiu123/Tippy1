4964e0fd3b5fb9828b13cf905266999c5c447146
LexuanLiu123 <lel006@ucsd.edu>
2020-01-23 19:36:45 -0800

    Initial Commit

---
+++ Tippy1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Tippy1
+//
+//  Created by Judy Liu on 2020-01-23.
+//  Copyright © 2020 Judy Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double (billField.text!) ?? 0
+        let tipPercent = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercent[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        tipLabel.text = String (format: "$%.2f", tip)
+        totalLabel.text = String (format: "$%.2f", total)
+    }
+    
+    
+
+}
+
